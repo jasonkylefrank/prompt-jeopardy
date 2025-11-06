3bb91e16580d4cc147e768fd9776393be23eeae6
Jason Frank <jasonkylefrank@gmail.com>
2025-11-06 15:25:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -31,9 +31,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devIndicators: {
-    buildActivity: false,
-  },
   // In a future major version of Next.js, you will need to explicitly configure "allowedDevOrigins" in next.config to allow cross-origin requests.
   // Read more: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
   allowedDevOrigins: [
